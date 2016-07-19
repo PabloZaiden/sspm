b6610095def1b0c259273d145f408692fb0299a1
Pablo Zaidenvoren <pablo@zaiden.com.ar>
2016-07-19 17:32:25 -0300

    enforce SSL

--- SelfServicePasswordManagement/Global.asax.cs
+++ SelfServicePasswordManagement/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,12 +11,27 @@ namespace SelfServicePasswordManagement
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static bool EnforceHTTPS = false;
+
         protected void Application_Start()
         {
+            if (ConfigurationManager.AppSettings["EnforceHTTPS"] == "true")
+            {
+                EnforceHTTPS = true;
+            }
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (EnforceHTTPS && !Context.Request.IsSecureConnection)
+            {
+                Response.Redirect(Context.Request.Url.ToString().Replace("http:", "https:"));
+            }
         }
     }
 }
